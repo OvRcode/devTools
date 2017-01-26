ac026cb0495bb1fa5a8924f6ffc3a671705549d6
Mike Barnard <michael.d.barnard@gmail.com>
2017-01-26 17:54:48 -0500

    Trying different sized image

--- scripts/dbImport.sh
+++ scripts/dbImport.sh
@@ -6,12 +6,13 @@ sleep 30
 until mysqladmin -hdb -u$REMOTE_DB_USER -p$REMOTE_DB_PASS ping > /dev/null 2>&1 && echo 1 || echo 0; do
   echo "Waiting for Maria Db to come up"
 done
-
+docker wait dbDownload
 echo "Connection successful"
 echo "Decompressing Backup"
 gunzip -f /db/ovrride.sql.gz
 echo "Starting Import"
 set -e
 
+mysql -hdb -u$REMOTE_DB_USER -p$REMOTE_DB_PASS -e 'CREATE DATABASE IF NOT EXISTS ovrride'
 pv -f /db/ovrride.sql | mysql -hdb -u$REMOTE_DB_USER -p$REMOTE_DB_PASS
 pv -f /ovr_dev.sql | mysql -hdb -u$REMOTE_DB_USER -p$REMOTE_DB_PASS
