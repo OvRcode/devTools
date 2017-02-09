60a82d4fbdc9137caff473c6b85f54a57036999b
Mike Barnard <michael.d.barnard@gmail.com>
2017-02-09 08:53:31 -0500

    Added checks to import to wait for database backup to download before importing

--- scripts/dbImport.sh
+++ scripts/dbImport.sh
@@ -6,7 +6,17 @@ sleep 30
 until mysqladmin -hdb -u$REMOTE_DB_USER -p$REMOTE_DB_PASS ping > /dev/null 2>&1 && echo 1 || echo 0; do
   echo "Waiting for Maria Db to come up"
 done
-docker wait dbDownload
+LOOPCOUNT=0
+until gunzip -tf /db/ovrride.sql.gz; do
+  LOOPCOUNT=$((LOOPCOUNT+1))
+  if [ $LOOPCOUNT -gt 30 ]; then
+    echo "Something went wrong, it's taking too long to download db!"
+    exit 126
+  else
+    echo "Still waiting for DB to download..."
+    sleep 10
+  fi
+done
 echo "Connection successful"
 echo "Decompressing Backup"
 gunzip -f /db/ovrride.sql.gz
